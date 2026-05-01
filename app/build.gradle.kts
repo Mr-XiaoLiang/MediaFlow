df9fb065682d2d58f28b08cbd8198cede87a6cf8
lollipop <liujian594@hotmail.com>
2026-05-01 12:57:14 +0800

    feat: V2.9.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.lollipop.mediaflow"
         minSdk = 31
         targetSdk = 36
-        versionCode = 2_08_00
-        versionName = "2.8.0"
+        versionCode = 2_09_00
+        versionName = "2.9.0"
     }
 
     buildTypes {
