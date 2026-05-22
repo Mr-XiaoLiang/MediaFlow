5346cb48508b48b64b134d0e7380e86cc268a981
lollipop <liujian594@hotmail.com>
2026-05-22 23:15:50 +0800

    feat: Vision V2.12.0 升级个版本

--- vision/build.gradle.kts
+++ vision/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.lollipop.mediaflow"
         minSdk = 31
         targetSdk = 36
-        versionCode = 2_11_00
-        versionName = "2.11.0"
+        versionCode = 2_12_00
+        versionName = "2.12.0"
     }
 
     buildTypes {
